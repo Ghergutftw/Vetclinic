7e94ffef548588b85d8b42129102a57182782e99
GHERGUT MADALIN <madalinghergut@gmail.com>
2023-12-10 21:44:04 +0200

    Discovery Server - Eureka

--- BE/animal-service/src/main/java/app/AnimalServiceApplication.java
+++ BE/animal-service/src/main/java/app/AnimalServiceApplication.java
@@ -2,9 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+
 public class AnimalServiceApplication {
 
 	public static void main(String[] args) {
