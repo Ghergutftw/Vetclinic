60735cb0b8af6ae8b776db897b78a97c0ff898dd
GHERGUT MADALIN <madalinghergut@gmail.com>
2023-12-20 23:55:09 +0200

    Preparing for enviorment everything

--- BE/animal-service/src/main/java/app/controller/AnimalController.java
+++ BE/animal-service/src/main/java/app/controller/AnimalController.java
@@ -5,6 +5,7 @@ import app.entity.Animal;
 import app.service.AnimalService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
