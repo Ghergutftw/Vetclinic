ed208e2cd961ab4503d603e15b3a7da542dfbd4e
GHERGUT MADALIN <madalinghergut@gmail.com>
2023-12-22 20:24:15 +0200

    Generate a word document

---
+++ FE/src/environments/config-loader.ts
@@ -0,0 +1,15 @@
+// import * as yaml from 'js-yaml';
+// import * as fs from "fs";
+//
+// export class ConfigLoader {
+//   static loadConfig(): any {
+//     try {
+//       const fileContents = fs.readFileSync("../../../BE/application-development.yml", 'utf8');
+//       const config = yaml.load(fileContents);
+//       return config;
+//     } catch (e) {
+//       console.error('Error loading YAML file:', e);
+//       return null;
+//     }
+//   }
+// }
